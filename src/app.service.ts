025897c43355ef1afe24d3d5b5ded6ef696b3579
Sureshkannan <sureshkannan.muthusamy@aa.com>
2022-12-14 15:58:07 -0500

    adding download method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import fs = require('fs');
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
   getHello(): string {
     return 'Hello World!';
   }
@@ -11,6 +13,7 @@ export class AppService {
     // Extract the required URL
     const urls = this.extractUrlFromFile(file);
     console.log(`Found total ${urls.length}`);
+    // this.downloadFile(urls[0]);
     // Download the File ..
     return 'Hello World!';
   }
@@ -31,5 +34,20 @@ export class AppService {
     } catch (error) {
       console.error(`extractUrlFromFile - Error processing, ${error}`);
     }
+  }
+
+  private downloadFile(url: string) {
+    const fileName = url.split('clips/')[1].split('?')[0];
+    console.log('this is ', fileName);
+    const writer = fs.createWriteStream(`../../${fileName}.ts`);
+    this.httpService
+      .get(url, { responseType: 'stream' })
+      .subscribe((response) => {
+        response.data.pipe(writer);
+        return new Promise((resolve, reject) => {
+          writer.on('finish', resolve);
+          writer.on('error', reject);
+        });
+      });
   }
 }
